03e131f4f46f9a8a314ee71db4f2f5c64e5053c0
Wenyue <wangwenyue1993@gmail.com>
2018-05-06 01:02:07 +0800

    add some files

---
+++ sweepFunc.js
@@ -0,0 +1,338 @@
+var log = console.log.bind(console)
+
+var e = (selector) => {
+    let ele = document.querySelector(selector)
+    if (! ele) {
+        alert(`${selector} not founded`)
+    } else {
+        return ele
+    }
+}
+
+var checkEdge = (x, y, row, col) => {
+    return x >= 0 && x < col && y >= 0 && y < row
+}
+
+var initMap = (row, col, mineNum) => {
+    let map = []
+
+    var initRow = () => {
+        for (let i = 0; i < row; i++) {
+            map[i] = []
+        }
+    }
+
+    var initCol = () => {
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                map[i][j] = 0
+            }
+        }
+    }
+
+    var initBlankMap = () => {
+        initRow(row)
+        initCol(row, col)
+    }
+
+    var randomMine = () => {
+        var randomSpot = () => {
+            let x = Math.floor(Math.random() * row)
+            let y = Math.floor(Math.random() * col)
+            // log('x, y:', x, y)
+            if (map[x][y] != 9) {
+                map[x][y] = 9
+            } else {
+                randomSpot()
+            }
+        }
+        for (let i = 0; i < mineNum; i++) {
+            randomSpot()
+        }
+    }
+    // initFinalMap只适合正方形地图生成，这是个Bug
+    var initFinalMap = () => {
+        let dx = [0, 1, -1, 0, 1, -1, -1, 1]
+        let dy = [1, 0, 0, -1, 1, -1, 1, -1]
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                if (map[i][j] != 9) {
+                    for (let t = 0; t <= 8; t++) {
+                        let x = i + dx[t]
+                        let y = j + dy[t]
+                        if (checkEdge(x, y, row, col) && map[x][y] == 9) {
+                            map[i][j] += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var makeMap = () => {
+        initBlankMap(row, col)
+        log('initBlankMap successed')
+        randomMine(row, col, mineNum)
+        log('randomMine successed')
+        initFinalMap(row, col, map)
+        log('initFinalMap successed')
+    }
+
+    makeMap()
+    //log('map:', map)
+    return map
+}
+
+// var initStatics = (mineNum, endTime) => {
+//     let mineLeft = e('.mineLeft')
+//     mineLeft.innerText = `${mineNum}`
+//
+//     let time = e('.tick')
+//     time.innerText = '0'
+//     let i = 1
+//     //重开游戏的时候清除上一盘的endTime
+//     clearInterval(endTime)
+//     isBoom = 0
+//     endTime = setInterval(() => {
+//         time.innerHTML = `${i++}`
+//     }, 1000)
+// }
+
+var renderHtml = (map) => {
+
+    var renderRow = () => {
+        let x = e('.gameBox')
+        for (let i = 0; i < map.length; i++) {
+            x.innerHTML +=
+            `<ul class="row x-${i}" data-x="${i}"></ul>`
+        }
+    }
+
+    var renderCol = () => {
+        let y = document.querySelectorAll('.row')
+        for (let i = 0; i < y.length; i++) {
+            for (let j = 0; j < map[0].length; j++) {
+                let p = map[i][j]
+                if (p == 0) {
+                    p = ''
+                }
+                y[i].innerHTML +=
+                `<li class="col y-${j} num-${p}" data-y="${j}">
+                    <span>${p}</span>
+                    <img src="flag.svg" class="img-flag hide">
+                </li>`
+            }
+        }
+    }
+
+    var renderMap = () => {
+        renderRow()
+        renderCol()
+    }
+
+    renderMap()
+}
+
+var sweepMine = (row, col, mineNum, isBoom) => {
+    let safeCell = 0
+
+    var checkWin =  (safeCell) => {
+        //如果没踩到炸弹，并且safeCell数量合适
+        if (safeCell === (row * col - mineNum) && isBoom === 0) {
+            log('checkWin 被执行了')
+            alert('You win')
+        }
+    }
+
+    var clearCell = (x, y) => {
+        if (checkEdge(x, y, row, col)) {
+            //找到点击的cell
+            // log('x3, y3:', x, y)
+            let cell = e(`.x-${x}`).children[y]
+            //log('cell->',cell)
+            if (cell.style.background !== 'white') {
+                cell.style.background = 'white'
+                //透明度还原为1，全显示
+                cell.children[0].style.opacity = '1'
+                //下面这行代码是很有必要的，虽然所有img已经给了hide
+                //但是已经插上旗子的cell，如果是空cell, 被点击了，应该hide旗子
+                //所以要加上hide,确保旗子会被隐藏
+                cell.children[1].classList.add('hide')
+                safeCell ++
+                // log('safeCell Number:', safeCell)
+                checkWin(safeCell)
+                //如果innerText为空，则继续遍历
+                if (cell.innerText === '') {
+                    //log('inside cell.innerText now')
+                    checkAroundMine(x, y)
+                }
+            }
+        }
+    }
+
+    var checkAroundMine = (x, y) => {
+        // log('x2, y2:', x, y)
+        let dx = [0, 1, -1, 0, 1, -1, -1, 1]
+        let dy = [1, 0, 0, -1, 1, -1, 1, -1]
+        for (let i = 0; i < 8; i++) {
+            let newX = x + dx[i]
+            let newY = y + dy[i]
+            clearCell(newX, newY)
+        }
+    }
+
+    //TODO classlist.contains可以用has函数优化
+    //TODO 错误的想法：self不支持self.e('img')来选中元素，而前面的cell可以支持cell.e('img')
+    //怀疑是self = event.target 的原因
+    var bindCells = () => {
+        let row = document.querySelectorAll('.row')
+        var bindCell = (row) => {
+            for (let i = 0; i < row.length; i++) {
+                //每一行都绑定事件
+                row[i].addEventListener('click', (event) => {
+                    let self = event.target
+                    //<li> </li>的tagName是 'LI'
+                    if (self.tagName != 'LI') {
+                        //如果点击到了span上
+                        //则后续的操作不会有反应，所以要绑定到父节点li上
+                        self = self.parentElement
+                        //log('点击到了span，换成li')
+                    }
+                    //处理已经被插了旗子的cell，让cell不能被点击
+                    let spanTag = self.children[0]
+                    let imgTag = self.children[1]
+                    let flag = imgTag.classList.contains('hide')
+
+                    if (self.tagName === 'LI' && flag) {
+                        if (spanTag.innerText === '9') {
+                            isBoom = 1
+                            let boomCells = document.querySelectorAll('.num-9')
+                            //log('boomCells ==>', boomCells)
+                            for (let i = 0; i < boomCells.length; i++) {
+                                // log('boomCell-->', boomCells[i])
+                                boomCells[i].classList.add('boom')
+                            }
+                            alert('Game Over, you lose.')
+                        } else if (spanTag.innerText !== 9 && self.style.background !== 'white') {
+                            //log('still alive')
+                            spanTag.style.opacity = '1'
+                            self.style.background = 'white'
+                            safeCell ++
+                            checkWin(safeCell)
+                        }
+                        //点击的span为空，则提取这个被点击cell的 x, y 坐标，遍历扫雷
+                        if (self.children[0].innerText === '') {
+                            //这里必须要parseInt，不然后面坐标相加的时候会报错
+                            let x = parseInt(self.parentElement.dataset.x)
+                            let y = parseInt(self.dataset.y)
+                            // log('x1, y1', x, y)
+                            checkAroundMine(x, y)
+                        }
+                    }
+                })
+            }
+        }
+        //处理一下 Mines left的显示，以及鼠标右键插入旗子的问题
+        var bindFlag = (row) => {
+            for (let i = 0; i < row.length; i++) {
+                //TODO 这里必须用var 不能用let 否则地雷计数有问题？？？？
+                // 怀疑与addEventListener 有关系
+                var mineNumNow = mineNum
+                row[i].addEventListener('contextmenu', function (event) {
+                    event.preventDefault();
+                    var self = event.target
+                    if (self.tagName != 'LI') {
+                        self = event.target.parentElement
+                    }
+                    if (self.tagName === 'LI') {
+                        let imgTagClasslist = self.children[1].classList
+                        let mineLeft = e('.mineLeft')
+                        //插旗子操作
+                        if (imgTagClasslist.contains('hide') && self.style.background !== 'white') {
+                            if (mineNumNow !== 0) {
+                                mineNumNow--
+                            }
+                            imgTagClasslist.remove('hide')
+                        } else if (self.style.background !== 'white') {
+                            //取消旗子操作
+                            imgTagClasslist.add('hide')
+                            mineNumNow++
+                        }
+                        mineLeft.innerText = `${mineNumNow}`
+                    }
+                })
+            }
+        }
+
+        bindCell(row)
+        bindFlag(row)
+    }
+    bindCells()
+}
+var stopTime
+var gameStart = (row, col, mineNum) => {
+    var mineLeft = e('.mineLeft')
+    mineLeft.innerText = `${mineNum}`
+    var time = e('.tick')
+    time.innerText = `0`
+    var i = 1
+    clearInterval(stopTime)
+    stopTime = setInterval(() => {
+        //log('时间', i)
+        time.innerText = `${i++}`
+    }, 1000)
+
+    isBoom = 0
+    // 清除原来的地图
+    var box = e('.gameBox')
+    box.innerHTML = ''
+    var map =initMap(row, col, mineNum)
+    renderHtml(map)
+    sweepMine(row, col, mineNum)
+}
+
+var levelSelector = () => {
+    var level = document.querySelectorAll('.choice-level')
+    for (let i = 0; i < level.length; i++) {
+        //TODO 非正方形地图会有bug。生成失败
+        level[i].addEventListener('click', (event) => {
+            var self = event.target
+            if (self.innerHTML === 'Fresh') {
+                row = 9
+                col = 9
+                mineNum = 10
+                log('Fresh clicked',mineNum)
+                gameStart(row, col, mineNum)
+            } else if (self.innerHTML === 'Normal') {
+                row = 16
+                col = 16
+                mineNum = 40
+                log('Normal clicked',mineNum)
+                gameStart(row, col, mineNum)
+            } else if (self.innerHTML === 'Hell') {
+                row = 30
+                col = 30
+                mineNum = 99
+                log('Hell clicked',mineNum)
+                gameStart(row, col, mineNum)
+            }
+        })
+    }
+    let restart = e('.restart')
+    restart.addEventListener('click', (event) => {
+        console.log('restart clicked', mineNum)
+        gameStart(row, col, mineNum)
+    })
+}
+
+var isBoom = 0
+var row = 16
+var col = 16
+var mineNum = 40
+
+var __main = () => {
+    levelSelector()
+    gameStart(row, col, mineNum)
+}
+
+__main()
